b03a7eeaf88bba8a9e6c991560b67b4435c47d9d
jenniferchristine <jeja2306@student.miun.se>
2024-09-10 12:38:44 +0200

    added interface and types for objects

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,2 +1,11 @@
 export class Course {
+    /*courseCode: string;
+    subjectCode: string;
+    level: string;
+    progression: string;
+    courseName: string;
+    points: number;
+    institutionCode: string;
+    subject: string;
+    syllabus: string;*/
 }
