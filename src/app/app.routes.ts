9c26050468baf08d67a43a79ff12cacf57ceab18
jenniferchristine <jeja2306@student.miun.se>
2024-09-11 11:55:46 +0200

    found issue: forgot to provide httpclient to config app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CoursesComponent } from './courses/courses.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "courses", component: CoursesComponent },
     { path: "schedule", component: ScheduleComponent },
     { path: "404", component: NotFoundComponent }, 
-    { path: "**", component: NotFoundComponent },
-    { path: "", redirectTo: '/home', pathMatch: 'full' }
+    { path: "**", redirectTo: "404" },
+    { path: "", redirectTo: "/home", pathMatch: "full" }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule] 
+})
+export class AppRoutingModule { } 
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../models/course';
 
@@ -10,7 +10,7 @@ export class CourseService {
   private url: string = "https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
   constructor(private http: HttpClient) { }
 
-  getPosts() : Observable<any[]> {
-    return this.http.get<any[]>(this.url);
+  getPosts() : Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
   }
 }
