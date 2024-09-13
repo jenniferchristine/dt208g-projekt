a15e368a46f8775f533bf521156098faf8f80372
jenniferchristine <jeja2306@student.miun.se>
2024-09-13 21:45:17 +0200

    trying to add responsive menu through ngfor but its not working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
 
+  /* toggla responsiv meny */
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    console.log("menu toggled");
+  }
 }
